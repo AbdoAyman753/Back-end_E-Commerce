6e555e5266b45352f617f2a9855fafe0eb936590
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-06-18 18:36:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const errorHandler = require("express-async-error").Handler;
+// DB connection
+require("./db");
+//Requiring Routes
+// const usersRouter = require("./src/routes/usersRoutes");
+
+
+// parsing incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// error handler over any async function
+app.use(errorHandler());
+
+// Routes
+// app.use("/users", usersRouter);
+
+
+app.use((req, res, next) => {
+  res.send("<h1 style='text-align:center'>Hello World</h1>");
+});
+
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).send({
+    status: statusCode,
+    message: err?.message || "Internal Server Error!",
+    errors: err?.errors || []
+  });
+});
+
+app.listen(8000);
