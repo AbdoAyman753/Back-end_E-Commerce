1f0013b10074b11deea8950273c60a724996f14c
Hossam_Tarek <hosamtarek4444@gmail.com>
2023-06-20 19:39:31 +0200

    I just added the product Router ( the other changes because we need to standardize the single quotes in VS Code )

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const errorHandler = require("express-async-error").Handler;
-require("dotenv").config();
+const errorHandler = require('express-async-error').Handler;
+require('dotenv').config();
 // DB connection
-require("./db");
+require('./db');
 //Requiring Routes
 // const usersRouter = require("./src/routes/usersRoutes");
-
+const productRouter = require('./src/routes/productsRoutes');
 
 // parsing incoming requests
 app.use(express.json());
@@ -17,20 +17,19 @@ app.use(errorHandler());
 
 // Routes
 // app.use("/users", usersRouter);
-
+app.use('product', productRouter);
 
 app.use((req, res, next) => {
   res.send("<h1 style='text-align:center'>Hello World</h1>");
 });
-
 
 // Global error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   res.status(statusCode).send({
     status: statusCode,
-    message: err?.message || "Internal Server Error!",
-    errors: err?.errors || []
+    message: err?.message || 'Internal Server Error!',
+    errors: err?.errors || [],
   });
 });
 
