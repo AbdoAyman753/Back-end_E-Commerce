8ad057fffa85948ee03f326bd1f293d37d1369ef
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-06-21 15:16:20 +0300

    created uploading profile pic

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,8 @@ const AppError = require("../utils/AppError");
 
 const jwt = require("jsonwebtoken");
 
+const { uploader } = require("../utils/config/cloudinaryConfig");
+
 const getAllUsers = async (req, res, next) => {
 	const users = await User.find();
 	res.send(users);
@@ -43,6 +45,36 @@ const updateUser = async (req, res, next) => {
 	res.send({ message: "user updated successfully!", editedUser });
 };
 
+const uploadUserPic = async (req, res, next) => {
+	const { id } = req.params;
+	const user = await User.findById(id);
+	if (!user) return next(new AppError("User Not Found", 404));
+
+	if (req.file) {
+		const file = dataUri(req).content;
+		return uploader
+			.upload(file)
+			.then(async (result) => {
+				const image = result.url;
+				await User.findByIdAndUpdate(id, { profile_pic:image });
+				return res.status(200).json({
+					messge: "Your image has been uploded successfully to cloudinary",
+					data: {
+						image,
+					},
+				});
+			})
+			.catch((err) =>
+				res.status(400).json({
+					messge: "someting went wrong while processing your request",
+					data: {
+						err,
+					},
+				})
+			);
+	}
+};
+
 const deleteUser = async (req, res, next) => {
 	const { id } = req.params;
 	const user = await User.findById(id);
@@ -64,7 +96,10 @@ const login = async (req, res, next) => {
 	if (!isMatch) return next(new AppError("Invalid Credentials!", 400));
 
 	// creating a token
-	const token = jwt.sign({ id: user._id, role: user.role }, process.env.ENCRYPTION_KEY);
+	const token = jwt.sign(
+		{ id: user._id, role: user.role },
+		process.env.ENCRYPTION_KEY
+	);
 
 	user.password = undefined;
 	res.send({ message: "user logged in successfully!", user, token });
@@ -75,6 +110,7 @@ module.exports = {
 	getUserById,
 	register,
 	updateUser,
+	uploadUserPic,
 	deleteUser,
 	login,
 };
